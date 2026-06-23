cf9ff114a667d175c6c233f2fd2fa8b929153c79
Srinivasa Rao Talari <143414405+Srinivasaraot20@users.noreply.github.com>
2026-06-23 15:37:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ class Config:
     # Application configuration
     DEBUG = False
     TESTING = False
-    
+    SITE_URL = os.environ.get('SITE_URL', 'https://srinivas-profile.onrender.com')
     # Logging configuration
     LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
     LOG_FILE = os.environ.get('LOG_FILE')
